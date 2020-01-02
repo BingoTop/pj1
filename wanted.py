57e320bca4850e4913c4d345fccb47e33b5461a2
정재원 <40786401+BingoTop@users.noreply.github.com>
2020-01-03 00:41:22 +0900

    Update wanted.py

--- wanted.py
+++ wanted.py
@@ -18,15 +18,16 @@ options.add_argument("disable-gpu")
 # cur = con.cursor()
 # ---------------------
 
+
 def iframe_checker(url):
     req = requests.get(url)
 
-    driver = webdriver.Chrome("C:/chromedriver",chrome_options=options)
+    driver = webdriver.Chrome("/home/jaewon/다운로드/chromedriver", chrome_options=options)
     driver.get(url)
     iframe = driver.find_elements_by_tag_name('iframe')
     print(iframe)
     if iframe != None:
-        for i,iframe in enumerate(iframe):
+        for i, iframe in enumerate(iframe):
             try:
                 print(driver.page_source)
                 print(f"{i}번쨰 iframe 입니다.")
@@ -37,58 +38,69 @@ def iframe_checker(url):
                 print(f"pass by except: iframes[{i}]")
                 pass
 
+
 class WantedHandler:
-    def __init__(self,url):
+    def __init__(self, url):
         self.url = url
-        self.driver = webdriver.Chrome("C:/chromedriver")
-    
+
+        self.driver = webdriver.Chrome("/home/jaewon/다운로드/chromedriver")
+
     def Parse(self):
-        req = requests.get(self.url)
-        soup = BeautifulSoup(req.content,'html.parser')
-        li_tag = soup.select("ul.clearfix")
-        print(li_tag)
+        # req = requests.get(self.url)
+        self.driver.get(self.url)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        titles = soup.select("ul.clearfix > li > div > a  > div > dl > dt")
+        companies = soup.select("ul.clearfix > li > div > a  > div > dl > dd")
+        
+        for title in titles:
+            print(title.text)
+        for company in companies:
+            print(company.text)
+        
+
+
 
 class RocketPunchHandler:
-    def __init__(self,url):
+    def __init__(self, url):
         self.url = url
-        self.driver = webdriver.Chrome("C:/chromedriver")
-    
+        self.driver = webdriver.Chrome("/home/jaewon/다운로드/chromedriver")
+
     def Parse(self):
         req = requests.get(self.url)
-        soup = BeautifulSoup(req.content,'html.parser')
+        soup = BeautifulSoup(req.content, 'html.parser')
         li_tag = soup.select("#company-list > div")
         print(li_tag)
 
+
 class ProgrammersHandler:
-    def __init__(self,url):
+    def __init__(self, url):
         self.url = url
-        # self.driver = webdriver.Chrome("C:/chromedriver")
+        # self.driver = webdriver.Chrome("/home/jaewon/다운로드/chromedriver")
         self.count = 1
 
         # ----sqlite3 설정--------
         self.con = sqlite3.connect("Programmers.db")
         self.cur = self.con.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS jobs(id integer PRIMARY KEY autoincrement, job CHAR(32),address text,experience CHAR(32))")
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS jobs(id integer PRIMARY KEY autoincrement, job CHAR(32),address text,experience CHAR(32))")
         # -----------------------
-    
+
     def Parse(self):
         req = requests.get(self.url+str(self.count))
         print(url)
         while req.status_code == 200:
-            soup = BeautifulSoup(req.content,'html.parser')
+            soup = BeautifulSoup(req.content, 'html.parser')
             titles = soup.select("h4.position-title")
             companys = soup.select("h5.company-name")
             addresses = soup.select("li.location")
             experiences = soup.select("li.experience")
-            
+
             idx = 0
             for i in soup.select("li.list-position-item"):
-                idx +=1
-            
-            for num in range(0,idx):
-                pass
-            
+                idx += 1
 
+            for num in range(0, idx):
+                pass
 
             # print("---TITLE-----")
             # for title in titles:
@@ -108,18 +120,18 @@ class ProgrammersHandler:
             # for experiences in experiences:
             #     print(experiences.text)
             # print("---------------")
-            
+
             # self.count +=1
             break
 
 
 # iframe_checker("https://www.wanted.co.kr/wdlist/518/873?country=kr&job_sort=job.latest_order&years=-1&locations=all")
-# URL = "https://www.wanted.co.kr/wdlist/518/873?country=kr&job_sort=job.latest_order&years=-1&locations=all"
-# W = WantedHandler(URL)
-# W.Parse()
+URL = "https://www.wanted.co.kr/wdlist/518/873?country=kr&job_sort=job.latest_order&years=-1&locations=all"
+W = WantedHandler(URL)
+W.Parse()
 # url = "https://www.rocketpunch.com/jobs"
 # R = RocketPunchHandler(url)
 # R.Parse()
-# url = "https://programmers.co.kr/job?page="    
+# url = "https://programmers.co.kr/job?page="
 # P = ProgrammersHandler(url)
 # P.Parse()
